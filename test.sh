1b076dcbb4ebd231420bbbdd0f5b63b93b7319be
Denis Redozubov <denis.redozubov@gmail.com>
2020-04-02 20:45:17 +0300

    Fix tests

--- test.sh
+++ test.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bats
 
-exe="./dist-newstyle/build/x86_64-osx/ghc-8.6.5/dhall-default-0.1.0.0/x/dhall-default/build/dhall-default/dhall-default"
+exe="/usr/local/bin/dhall-default"
 
 for input in $(ls test/*.dhall); do
   @test $input {
-    $exe --filename $input
-    exe_output=$output
-    cat $input.result
-    [ "$output" = "$exe_output" ]
+    exe_output=$($exe --filename $input)
+    result_output=$(cat $input.result)
+    [ "$exe_output" = "$result_output" ]
   };
 done
